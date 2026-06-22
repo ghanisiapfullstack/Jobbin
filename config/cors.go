2cf5a1dfc18e61d7d254c1a061de5f722f23197b
ghanisiapfullstack <ghanisiapfullstack@gmail.com>
2026-06-22 12:26:21 +0700

    fix: update CORS allow production frontend URL

--- config/cors.go
+++ config/cors.go
@@ -9,7 +9,10 @@ func init() {
 	config.Add("cors", map[string]any{
 		"paths":                []string{"*"},
 		"allowed_methods":      []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		"allowed_origins":      []string{"*"},
+		"allowed_origins": []string{
+			"http://localhost:5173",
+			"https://jobbin-frontend.happygrass-ce5e6fc0.southeastasia.azurecontainerapps.io",
+		},
 		"allowed_headers":      []string{"Content-Type", "Authorization", "Accept", "X-Requested-With"},
 		"exposed_headers":      []string{},
 		"max_age":              86400,
